748a4092c9a9e24db42ab6e143f7f92fa6135cba
Preetham K H <blighter.soul@gmail.com>
2026-01-28 00:37:16 +1100

    Service Contract - IDataIngestionService Service for ingesting data from OpenDota API into the database

---
+++ src/DotaDashboard/Services/Interfaces/IDataIngestionService.cs
@@ -0,0 +1,29 @@
+﻿namespace DotaDashboard.Services.Interfaces;
+
+/// <summary>
+/// Service for ingesting data from OpenDota API into the database
+/// </summary>
+public interface IDataIngestionService
+{
+    /// <summary>
+    /// Ingest all Dota 2 heroes into the database
+    /// </summary>
+    /// <returns>Number of heroes ingested</returns>
+    Task<int> IngestHeroesAsync();
+
+    Task<int> IngestProMatchesAsync(int limit = 10);
+
+    ///// <summary>
+    ///// Ingest recent professional matches
+    ///// </summary>
+    ///// <param name="limit">Number of matches to fetch</param>
+    ///// <returns>Number of matches ingested</returns>
+    //Task<int> IngestPublicMatchesAsync(int limit = 50);
+
+    /// <summary>
+    /// Ingest details for a specific match
+    /// </summary>
+    /// <param name="matchId">The match ID</param>
+    /// <returns>1 if successful, 0 if failed or already exists</returns>
+    Task<int> IngestMatchDetailsAsync(long matchId);
+}
